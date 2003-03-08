e7d08d90d1c2cf18420a5cf696f8ac670d80736c
curt <curt>
2003-03-08 01:44:28 +0000

    A couple minor changes to this helper script.

--- src/Prep/DemChop/process-DEM-30.sh
+++ src/Prep/DemChop/process-DEM-30.sh
@@ -3,8 +3,8 @@
 
 TOOLPATH=`pwd`
 SOURCE=/stage/fgfs01/curt/RawData/DEM-30-Ascii
-SCRATCH=${SOURCE}/Scratch
-WORK=/stage/fgfs01/curt/Work/DEM-30
+SCRATCH=/tmp/Scratch
+WORK=/stage/fgfs04/curt/Work/DEM-30-New
 
 if [ -z $1 ]; then
     echo "usage: $0 base-dem-30-ascii-file"
@@ -21,11 +21,8 @@ echo "cd to $SCRATCH successful"
 tar xzvf ${SOURCE}/${1}.tar.gz
 
 for i in ${SCRATCH}/$1/*.dem; do
-    ${TOOLPATH}/demchop $i $WORK > ${TOOLPATH}/log 2>&1
+    ${TOOLPATH}/demchop $i $WORK >> ${TOOLPATH}/log-$1 2>&1
 done
 
 echo rm -rf ${SCRATCH}
 rm -rf ${SCRATCH}
-
-
-
