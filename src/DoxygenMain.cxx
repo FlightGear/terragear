4c975a05370f49eed87aff401745d3854c11d7f9
Peter Morgan <pete@daffodil.uk.com>
2012-09-26 05:34:39 +0100

    Doxygen and code documentation Small patch to add some documentation

---
+++ src/DoxygenMain.cxx
@@ -0,0 +1,55 @@
+/* This is a dummy code file that only contains doxygen main page
+   documentation.  It has a .cxx extension so that emacs will happily
+   autoindent correctly. */
+
+/** \mainpage TerraGear
+ *  \version   0.00a
+ * 
+ *  \copyright GNU Public License.
+ *
+ * \section Introduction
+ * TerraGear is a collection of open-source tools and rendering libraries which can transform publically available GIS data in 3D representations (i.e. 3D models or 3D maps) of the earth for use in real time rendering projects.
+ *  - Wiki Info at http://wiki.flightgear.org/TerraGear
+ *  - Source from https://gitorious.org/fg/terragear
+ * 
+
+ *
+ * TerraGear is a collection of tools for building scenery for the FlightGear project.  Generally, the process is done in two steps:
+ *
+ *  1. Preprocess the original raw data.  This chops up the data into
+ *    the FG tiling scheme and saves it in a simple, intermediate format.
+ * 
+ *  2. Collect all the different pieces of intermediate data and
+ *     assemble them into a 3d model of the terrain.
+ *
+ * From here: 
+ * - <a href="README">README</a> 
+ * - <a href="README.SimGear">README.howto</a>
+ * - <a href="README.SimGear">README.SimGear</a>
+ * - <a href="README.gpc">README.gpc</a>
+ * - <a href="README.cygwin">README.cygwin</a>
+ * - <a href="README.newmat">README.newmat</a>
+ * - <a href="TODO.txt">TODO.txt</a>
+ *
+ * \section supports Supported Platforms
+ * TerraGear is actively been built on the following platforms: TODO Jenkins Link
+ *
+ *   - Linux (x86)
+ *   - Windows (MSVC, Cygwin, Mingwin)
+ *   - SGI (native compilers)
+ *   - Mac OS X
+ *   - FreeBSD
+ 
+ * \section depends Dependencies
+ *
+ * TerraGear depends on
+ *
+ *   - <a href="/simgear"  target="_blank">SimGear</a>
+ *   - TODO
+ 
+ * \section license Licensing
+ *
+ * TerraGear is licensed under the terms of the << TODO_LEGAL_STUFF >>
+
+ */
+
